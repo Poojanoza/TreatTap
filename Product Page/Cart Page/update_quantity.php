204f1239abdc5fc58bc9ed8339dc97b76d2f81f1
poojanoza <poojanoza89@gmail.com>
2024-01-18 23:47:19 +0530

    cart backend changes.

--- Product Page/Cart Page/update_quantity.php
+++ Product Page/Cart Page/update_quantity.php
@@ -5,15 +5,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the product ID and new quantity from the POST data
     $productId = $_POST['product_id'];
     $newQuantity = $_POST['new_quantity'];
+    $totalProductPrice = $_POST['total_product_price'];
 
-    // Update the quantity in the database
-    $updateSql = "UPDATE cart SET quantity = $newQuantity WHERE product_id = $productId";
+    // Update the quantity and total price in the database
+    $updateSql = "UPDATE cart SET quantity = $newQuantity, total_price = $totalProductPrice WHERE product_id = $productId";
     $updateResult = $conn->query($updateSql);
 
     if ($updateResult) {
-        echo 'Quantity updated successfully';
+        echo 'Quantity and total price updated successfully';
     } else {
-        echo 'Error updating quantity';
+        echo 'Error updating quantity and total price';
     }
 } else {
     echo 'Invalid request method';
